a2309776a962fef10096e273c74eaf4852357361
khatiwadaprajwal <prajwalkhatiwada28@gmail.com>
2022-11-06 23:12:28 +0545

    semicolon added

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.listen(3001,"localhost",(err)=>{
     if(err){
         console.log("Server err")
     } else{
-        console.log("Server connected")
+        console.log("Server    connected");
     }
 })
